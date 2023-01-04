9abbb77a0c41e085a92cf5824d93780e325aa547
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-04 10:03:23 -0300

    refactor: move sut creation to a factory method

--- src/validation/validators/composite/composite-validation.spec.ts
+++ src/validation/validators/composite/composite-validation.spec.ts
@@ -1,18 +1,37 @@
 import { FieldValidationSpy } from '../mocks/mock-field-validation'
 import { ValidationComposite } from './composite-validation'
 
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidationsSpy: FieldValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const fieldValidationsSpy = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field')
+  ]
+  const sut = new ValidationComposite(fieldValidationsSpy)
+  return {
+    sut,
+    fieldValidationsSpy
+  }
+}
+
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field')
-    fieldValidationSpy.error = new Error('InvalidFieldError')
+    const { sut, fieldValidationsSpy } = makeSut()
+    fieldValidationsSpy[0].error = new Error('InvalidFieldError')
+    fieldValidationsSpy[1].error = new Error('SecondInvalidFieldError')
 
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('SecondInvalidFieldError')
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('InvalidFieldError')
+  })
 
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2
-    ])
+  test('Should return false if all validation succeeds', () => {
+    const { sut, fieldValidationsSpy } = makeSut()
+    fieldValidationsSpy[0].error = new Error('InvalidFieldError')
+    fieldValidationsSpy[1].error = new Error('SecondInvalidFieldError')
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe('InvalidFieldError')
   })
