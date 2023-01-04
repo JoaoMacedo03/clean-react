ac36b32a81938ced2b77ace554e838be898d26ea
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-04 11:32:52 -0300

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/builder/builder-validation.ts
@@ -0,0 +1,23 @@
+import { IFieldValidation } from '@/validation/contracts/field-validation'
+import { RequiredFieldValidation } from '@/validation/validators'
+
+export class ValidationBuilder {
+  private constructor (
+    private readonly fieldName: string,
+    private readonly validations: IFieldValidation[]
+
+  ) {}
+
+  static field (fieldName: string): ValidationBuilder {
+    return new ValidationBuilder(fieldName, [])
+  }
+
+  required (): ValidationBuilder {
+    this.validations.push(new RequiredFieldValidation(this.fieldName))
+    return this
+  }
+
+  build (): IFieldValidation[] {
+    return this.validations
+  }
+}
