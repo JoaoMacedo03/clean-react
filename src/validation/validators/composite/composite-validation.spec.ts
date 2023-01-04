362d7ac147cba2a7eeffdcfd2e918cb1613f4a20
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-04 09:54:19 -0300

    test: ensure ValidationComposite return error if any validation fails

---
+++ src/validation/validators/composite/composite-validation.spec.ts
@@ -0,0 +1,17 @@
+import { FieldValidationSpy } from '../mocks/mock-field-validation'
+import { ValidationComposite } from './composite-validation'
+
+describe('ValidationComposite', () => {
+  test('Should return error if any validation fails', () => {
+    const fieldValidationSpy = new FieldValidationSpy('any_field')
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+    fieldValidationSpy2.error = new Error('InvalidFieldError')
+
+    const sut = new ValidationComposite([
+      fieldValidationSpy,
+      fieldValidationSpy2
+    ])
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('InvalidFieldError')
+  })
+})
