43a4a5be9cb8a476e3530ecbb2eb1ac925788e84
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-04 09:56:05 -0300

    test: ensure ValidationComposite returns the first error found

--- src/validation/validators/composite/composite-validation.spec.ts
+++ src/validation/validators/composite/composite-validation.spec.ts
@@ -4,8 +4,10 @@ import { ValidationComposite } from './composite-validation'
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('InvalidFieldError')
+
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('InvalidFieldError')
+    fieldValidationSpy2.error = new Error('SecondInvalidFieldError')
 
     const sut = new ValidationComposite([
       fieldValidationSpy,
