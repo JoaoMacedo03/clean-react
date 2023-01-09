1944c7b8b5dfd8b55305e216a242013c02bd8490
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-09 12:42:23 -0300

    test: change test to use faker

--- src/main/test/cypress/e2e/integration/login.spec.ts
+++ src/main/test/cypress/e2e/integration/login.spec.ts
@@ -98,8 +98,8 @@ describe('Login', () => {
                 accessToken: faker.random.words()
             }
         })
-        cy.getByTestId('email').focus().type('mango@gmail.com')
-        cy.getByTestId('password').focus().type('12345')
+        cy.getByTestId('email').focus().type(faker.internet.email())
+        cy.getByTestId('password').focus().type(faker.random.alphaNumeric(9))
         cy.getByTestId('submit').click()
         cy.getByTestId('spinner').should('not.exist')
         cy.getByTestId('main-error').should('not.exist')
