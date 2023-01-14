8de8ab5b4231292f562d2c33397dfab9f37164fe
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-14 14:17:13 -0300

    feat: apply private route SurveyList page

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -4,6 +4,7 @@ import { MakeLogin, MakeSignUp } from '@/main/factories/pages'
 import { ApiContext } from '@/presentation/contexts'
 import { SurveyList } from '@/presentation/pages'
 import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '../adapters/current-account-adapter'
+import { PrivateRoute } from '@/presentation/components'
 
 const Router: React.FC = () => {
   return (
@@ -17,7 +18,9 @@ const Router: React.FC = () => {
         <Routes>
           <Route path='/login' caseSensitive element={<MakeLogin />} />
           <Route path='/signup' caseSensitive element={<MakeSignUp />} />
-          <Route path='/' caseSensitive element={<SurveyList />} />
+          <Route caseSensitive path='/' element={<PrivateRoute />}>
+            <Route path='/' caseSensitive element={<SurveyList />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </ApiContext.Provider>
