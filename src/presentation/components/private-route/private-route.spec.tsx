a856aa94254806b30279d6c58559cf9bba392eb2
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-14 14:11:52 -0300

    test: ensure PrivateRoute renders component if token is not empty

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -4,28 +4,37 @@ import { createMemoryHistory, MemoryHistory } from 'history'
 import PrivateRoute from './private-route'
 import { render } from '@testing-library/react'
 import { SurveyList } from '@/presentation/pages'
+import { ApiContext } from '@/presentation/contexts'
+import { mockAccountModel } from '@/domain/mocks'
 
 type SutTypes = {
     history: MemoryHistory
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (account = mockAccountModel()): SutTypes => {
     const history = createMemoryHistory({ initialEntries: ['/'] })
     render(
-        <Router location={history.location} navigator={history}>
-            <Routes>
-                <Route caseSensitive path='/' element={<PrivateRoute />}>
-                    <Route path='/' caseSensitive element={<SurveyList />} />
-                </Route>
-            </Routes>
-        </Router>
+        <ApiContext.Provider value={{ getCurrentAccount: () => account }}>
+            <Router location={history.location} navigator={history}>
+                <Routes>
+                    <Route caseSensitive path='/' element={<PrivateRoute />}>
+                        <Route path='/' caseSensitive element={<SurveyList />} />
+                    </Route>
+                </Routes>
+            </Router>
+        </ApiContext.Provider>
     )
     return { history }
 }
 
 describe('PrivateRoute', () => {
     test('Should redirect to /login if accessToken is empty', () => {
-        const { history } = makeSut()
+        const { history } = makeSut(null)
         expect(history.location.pathname).toBe('/login')
+    })
+
+    test('Should render current component if token is not empty', () => {
+        const { history } = makeSut()
+        expect(history.location.pathname).toBe('/')
     })
 })
