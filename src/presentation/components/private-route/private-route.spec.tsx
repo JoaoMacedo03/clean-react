fd79e3294ab5aa660324a4f7c00b12730f9efcd7
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-14 14:02:17 -0300

    refactor: move sut creation to a factory method

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import { Route, Router, Routes } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import PrivateRoute from './private-route'
 import { render } from '@testing-library/react'
 import { SurveyList } from '@/presentation/pages'
 
+type SutTypes = {
+    history: MemoryHistory
+}
+
+const makeSut = (): SutTypes => {
+    const history = createMemoryHistory({ initialEntries: ['/'] })
+    render(
+        <Router location={history.location} navigator={history}>
+            <Routes>
+                <Route caseSensitive path='/' element={<PrivateRoute />}>
+                    <Route path='/' caseSensitive element={<SurveyList />} />
+                </Route>
+            </Routes>
+        </Router>
+    )
+    return { history }
+}
+
 describe('PrivateRoute', () => {
     test('Should redirect to /login if accessToken is empty', () => {
-        const history = createMemoryHistory({ initialEntries: ['/'] })
-        render(
-            <Router location={history.location} navigator={history}>
-                <Routes>
-                    <Route caseSensitive path='/' element={<PrivateRoute />}>
-                        <Route path='/' caseSensitive element={<SurveyList />} />
-                    </Route>
-                </Routes>
-            </Router>
-        )
+        const { history } = makeSut()
         expect(history.location.pathname).toBe('/login')
     })
 })
