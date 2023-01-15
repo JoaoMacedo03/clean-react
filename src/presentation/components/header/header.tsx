efe1dc0469438eedaae08cce182a172ae01ed872
Joao Macedo <joaovictormacedo91@gmail.com>
2023-01-14 23:20:24 -0300

    feat: ensure Logout calls setCurrentAccount with undefined

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,15 +1,25 @@
-import React, { memo } from 'react'
+import React, { memo, useContext } from 'react'
 import { Logo } from '@/presentation/components'
 import Styles from './header-styles.scss'
+import { ApiContext } from '@/presentation/contexts'
+import { useNavigate } from 'react-router-dom'
 
 const Header: React.FC = () => {
+    const naviagate = useNavigate()
+    const { setCurrentAccount } = useContext(ApiContext)
+    const logout = (event: React.MouseEvent<HTMLAnchorElement>): void => {
+        event.preventDefault()
+        setCurrentAccount(undefined)
+        naviagate('/login')
+    }
+
     return (
         <header className={Styles.headerWrap}>
             <div className={Styles.headerContent}>
                 <Logo />
                 <div className={Styles.logoutWrap}>
                     <span>Joao Macedo</span>
-                    <a href="#">Sair</a>
+                    <a data-testid="logout" href="#" onClick={logout}>Sair</a>
                 </div>
             </div>
         </header>
